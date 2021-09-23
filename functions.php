2777287aeffa238486b870ac3325a237a6813a91
dufva <sussiedufva@hotmail.com>
2021-09-23 12:11:30 +0200

    another hook to change woocommerce added

--- functions.php
+++ functions.php
@@ -14,23 +14,34 @@ function my_theme_enqueue_styles() {
     );
 }
 
-function add_themes_features() {
+function add_themes_features() { //för bilder 
     add_theme_support('post-thumbnail');
 }
 add_action('after_setup_theme', 'add_themes_features'); 
 
-function myname(){
+function myname(){ //krokar in min namn i footer
     echo '<p class="container"> Sida av Susanna Dufva </p>';
 }
 add_action('blossom_shop_after_footer', 'myname');
 
-function my_footer(){
+function my_footer(){ //överskrivning av mallfil
     echo "test";
 }
 add_action('wp_footer', 'my_footer');
 
-function change_price($price){
-    $price .= ' per package';
+function change_price($price){ //justerar woocommerce via krok 
+    $price .= ' :-';
     return $price;
 }
 add_filter('woocommerce_get_price_html', 'change_price');
+
+add_filter( 'woocommerce_checkout_fields' , 'change_checkout_fields' );
+
+function change_checkout_fields( $fields ) { //justerar woocommerce via krok 
+     $fields['order']['order_comments']['placeholder'] = 'Här kan du t.ex skriva med portkod';
+    
+     return $fields;
+}
+
+
+
